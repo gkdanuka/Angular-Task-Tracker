412c6ffa0b6ef7db22fa24c80ebddf71a1692978
gkdanuka <gkdanuka@gmail.com>
2022-01-26 08:25:56 +0530

    Delete function fix

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -16,7 +16,7 @@ export class TaskService {
   }
 
   deleteTask(task: Task): Observable<Task>{
-    const url='${this.apiUrl}/${task.id}';
+    const url=this.apiUrl+'/'+task.id;
     return this.http.delete<Task>(url);
   }
 }
